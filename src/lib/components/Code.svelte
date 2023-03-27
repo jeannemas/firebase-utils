a6373d6ba7d5a648d2667d2872db5508e292a1ba
Jeanne Mas <jeannemas@jeannemas.me>
2023-03-27 16:23:05 -0400

    WIP

--- src/lib/components/Code.svelte
+++ src/lib/components/Code.svelte
@@ -1,12 +1,37 @@
 <script context="module" lang="ts">
+	import { onMount } from 'svelte';
 	import { JSONEditor, Mode, type JSONValue } from 'svelte-jsoneditor';
 </script>
 
 <script lang="ts">
 	export let value: JSONValue;
+
+	let wrapper: HTMLDivElement;
+	let editor: JSONEditor;
+
+	onMount(() => {
+		handleThemeChange();
+
+		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', handleThemeChange);
+	});
+
+	function handleThemeChange() {
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			wrapper.classList.add('jse-theme-dark');
+		} else {
+			wrapper.classList.remove('jse-theme-dark');
+		}
+
+		editor.refresh();
+	}
 </script>
 
-<div style="--jse-main-border: none;">
+<style lang="postcss">
+	/* load one or multiple themes */
+	@import 'svelte-jsoneditor/themes/jse-theme-dark.css';
+</style>
+
+<div class="jse-theme-dark" style="--jse-main-border: none;" bind:this="{wrapper}">
 	<JSONEditor
 		content="{{
 			text: undefined,
@@ -15,6 +40,7 @@
 		mainMenuBar="{false}"
 		mode="{Mode.tree}"
 		readOnly="{true}"
+		bind:this="{editor}"
 	/>
 </div>
 
